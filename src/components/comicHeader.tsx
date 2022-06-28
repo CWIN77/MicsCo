fa0699e3706fb0db2ee53b6a595e1ee858b9d14c
CWIN77 <cwstbp0426@gmail.com>
2022-06-28 17:31:35 +0900

    change comic detail ui

--- src/components/comicHeader.tsx
+++ src/components/comicHeader.tsx
@@ -1,23 +1,19 @@
 import styled from 'styled-components'
-import {ReactComponent as SVG_profile} from "../svgs/profile.svg"
 import {ReactComponent as SVG_arrow_left} from "../svgs/arrow_left.svg"
 
 const ComicHeader = () => {
   const svgProps = {width:26,height:26,fill:"#1A1A1A",style:{padding:8}}
 
   return (
-    <div>
-      <div style={{width:"100%",height:51.96}}/>
-      <Bar>
-        <div style={{position:"absolute",left:0}}>
-          <SVG_arrow_left {...svgProps} onClick={()=>{window.history.back()}} />
-        </div>
-        <div>
-          <Profile src={"https://lh3.googleusercontent.com/a-/AOh14GhNSjWAGbrfqbT6j186QBK8iPJBQIAQzCC6EOxheQ=s96-c"} />
-          <h2>CWIN77</h2>
-        </div>
-      </Bar>
-    </div>
+    <Bar>
+      <div style={{position:"absolute",left:0}}>
+        <SVG_arrow_left {...svgProps} onClick={()=>{window.history.back()}} />
+      </div>
+      <div>
+        <Profile src={"https://lh3.googleusercontent.com/a-/AOh14GhNSjWAGbrfqbT6j186QBK8iPJBQIAQzCC6EOxheQ=s96-c"} />
+        <h2>CWIN77</h2>
+      </div>
+    </Bar>
   )
 }
 
@@ -26,14 +22,15 @@ const Bar = styled.div`
   display:flex;
   align-items: center;
   justify-content: center;
-  position: fixed;
   z-index: 1;
-  top:0px;
   width:100vw;
   div{
     padding: 6px;
     display:flex;
     align-items: center;
+    h2{
+      font-size: 16px;
+    }
   }
 `
 const Profile = styled.img`
--- src/pages/ComicDetail.tsx
+++ src/pages/ComicDetail.tsx
@@ -1,18 +1,59 @@
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components'
 import ComicHeader from '../components/comicHeader';
-
+import {ReactComponent as SVG_book_open} from "../svgs/book_open.svg"
+import {ReactComponent as SVG_shield} from "../svgs/shield.svg"
+import {ReactComponent as SVG_tag} from "../svgs/tag.svg"
+import {ReactComponent as SVG_calendar} from "../svgs/calendar.svg"
+import {ReactComponent as SVG_people} from "../svgs/people.svg"
+import {ReactComponent as SVG_star} from "../svgs/star.svg"
 
 const ComicDetail = () => {
   const {id} = useParams();
-  
+  const iconProps = {width:20,height:20,fill:"#1A1A1A"}
+  const starIcon = {width:20,height:20,fill:"#ECD92D",style:{padding:4,marginRight:6,filter: "drop-shadow(0px 0px 0.2px rgba(0,0,0,0.25))"}}
   return (
     <Container>
       <ComicHeader />
       <ComicImg src=""/>
       <ComicInform>
         <Title>테스트 제목</Title>
+        <Explane>테스트 설명</Explane>
+        <TagContainer>
+          <Tag>
+            <SVG_book_open {...iconProps} />
+            <h2>연재중</h2>
+          </Tag>
+          <Tag>
+            <SVG_shield {...iconProps} />
+            <h2>안전</h2>
+          </Tag>
+          <Tag>
+            <SVG_tag {...iconProps} />
+            <h2>판타지</h2>
+            <h2>드라마</h2>
+          </Tag>
+        </TagContainer>
+        <TagContainer>
+          <Tag>
+            <SVG_calendar {...iconProps} />
+            <h2>22년 6월 연재 시작됨</h2>
+          </Tag>
+          <Tag>
+            <SVG_people {...iconProps} />
+            <h2>신도 마사유키</h2>
+          </Tag>
+        </TagContainer>
+        <AssessTop>
+          <h2>평가</h2>
+          <div>
+            <SVG_star {...starIcon} />
+            <h2>9.5 / 10</h2>
+          </div>
+        </AssessTop>
+        <AssessList>
 
+        </AssessList>
       </ComicInform>
     </Container>
   )
@@ -30,12 +71,58 @@ const ComicImg = styled.img`
   border-radius: 18px;
 `
 const ComicInform = styled.div`
+  height:100%;
   padding: 0px 1.5rem;
 `
 const Title = styled.h1`
   font-size: 24px;
   padding: 2px 0px;
 `
+const Explane = styled.h2`
+  font-size: 18px;
+  font-weight: 400;
+  opacity: 0.8;
+  margin-bottom: 30px;
+`
+const TagContainer = styled.div`
+  display:flex;
+  padding-top: 10px;
+  padding-bottom: 12px;
+`
+const Tag = styled.div`
+  opacity: 0.6;
+  display:flex;
+  align-items: center;
+  margin-right: 24px;
+  h2{
+    margin-left: 10px;
+    font-size: 15px;
+  }
+`
+const AssessTop = styled.div`
+  display:flex;
+  margin-top: 36px;
+  align-items: center;
+  justify-content: space-between;
+  h2{
+    font-size: 20px;
+  }
+  div{
+    display:flex;
+    align-items: center;
+    h2{
+      font-weight: 400;
+      font-size: 18px;
+      color:#ECD92D;
+      filter: drop-shadow(0px 0px 0.2px rgba(0,0,0,0.25));
+    }
+  }
+`
+const AssessList = styled.div`
+  display:flex;
+  flex-direction: column;
+`
+
 
 
 export default ComicDetail
